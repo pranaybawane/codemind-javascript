acaea94ab4efaf02028ed382df5ada2080ebf56c
Pranay bawane <pranaybawane1924@gmail.com>
2025-02-17 15:17:38 +0530

    01 variable Assignment 1
    01 variable Assignment 1

---
+++ 01-variable.js
@@ -0,0 +1,22 @@
+
+
+
+var age; // variable declaration
+
+console.log(age); // undefined
+
+age = "22"; // variable initialization  
+
+console.log(age);  
+
+var age = "22" ;  // variable declaration and intialization in same line
+console.log("AGE: ", age);
+
+var presentCity; // variable declaration 
+presentCity = "Pune"; // variable initialization 
+
+var presentCity = "Pune" ; // variable declaration and initialization in same line
+
+console.log("My Present City: ", presentCity);
+
+
---
+++ 01-variableAssign.js
@@ -0,0 +1,54 @@
+
+
+var myFullName; //  variable declaration
+
+console.log(myFullName);   
+
+myFullName = "Pranay Bawane" ;  // variable initialization
+
+console.log("My Full Name: ",myFullName);
+
+myFullName="Pranay Vinayakrao Bawane"
+console.log( "My Full Name: ", myFullName);
+
+var myLove= "I love only JS" ; // variable declaration and initialization in same line
+
+console.log("My Love: ",myLove);
+
+var firstName= "Pranay";
+
+console.log("First Name: ", firstName);
+
+var lastName= "Bawane";
+
+console.log("Last Name: ", lastName);
+
+var age= "26";
+
+console.log("Age: ", age);
+
+var address= "Amravati" ;
+
+console.log("Address: ", address);
+
+var pinCode= "444720";
+
+console.log("Pin Code: ", pinCode);
+
+var gender= "Male";
+
+console.log("Gender: ",gender);
+
+var isMarried ="No";
+
+console.log("Is Married: ", isMarried);
+
+
+
+
+
+
+
+
+
+
