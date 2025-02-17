b11184a1c05d9243e3ec53761f31b69def78931f
Pranay bawane <pranaybawane1924@gmail.com>
2025-02-17 20:14:19 +0530

    datatypes
    datatypes

---
+++ 002-datatype.js
@@ -0,0 +1,73 @@
+console.log("********Primitive Datatype*********");
+
+var age= 26; 
+console.log("Age: ", age);
+
+var typeOfAge= typeof(age);
+
+console.log("Type of Age: ", typeOfAge);
+
+var weight= 77;
+
+console.log("Weight: ",weight);
+
+
+var typeOfWeight=   typeof(weight);
+
+
+console.log("Type of weight: ", typeOfWeight);
+
+console.log("======Primitive Datatype String======");
+
+var city= "Pune";
+console.log("City: ", city);
+
+ var citiTypeOf=  typeof(city);
+
+ console.log("type of city: ", citiTypeOf);
+
+ console.log("======Primitive datatype Boolean=======");
+
+ var isMarried = false; 
+
+ console.log("Married Status: ", isMarried);
+
+ var isMarriedTypeOf= typeof(isMarried);
+
+ console.log("Type of is Married: ", isMarriedTypeOf);
+
+
+ console.log("======primitive datatype Special datatype=======");
+
+ var fullName ;
+
+ console.log("Before initialization Full Name: ",fullName);
+
+ var fullNameTypeOf= typeof(fullName);
+
+ console.log("Type of Full Name: ",fullNameTypeOf);
+
+
+ var pastAffairs= null;
+
+ console.log("Past Affairs: ",pastAffairs); 
+
+ var pastAffairsTypeof= typeof(null) ;
+
+ console.log("Type of Null: ", pastAffairsTypeof);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
